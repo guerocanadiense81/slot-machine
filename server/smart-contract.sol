f13e9b1e506d668bafd5c3f5f606b4ba6f72db4c
guerocanadiense81 <cedrickp1981@gmail.com>
2025-03-26 20:45:49 -0400

    Update smart-contract.sol

--- server/smart-contract.sol
+++ server/smart-contract.sol
@@ -9,20 +9,42 @@ contract METToken {
     mapping(address => uint256) public balanceOf;
 
     address public owner;
+    address public houseWallet;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
 
-    constructor(uint256 _initialSupply) {
+    constructor(uint256 _initialSupply, address _houseWallet) {
         owner = msg.sender;
+        houseWallet = _houseWallet;
         totalSupply = _initialSupply * 10 ** uint256(decimals);
         balanceOf[owner] = totalSupply;
     }
 
+    // Standard transfer function
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value, "Insufficient balance");
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    // Purchase tokens: players send BNB and MET tokens are transferred from house wallet to player
+    // Note: In practice, you'll need to handle payable and conversion; this is simplified.
+    function purchaseTokens(address buyer, uint256 metAmount) public returns (bool success) {
+        require(balanceOf[houseWallet] >= metAmount, "House wallet has insufficient tokens");
+        balanceOf[houseWallet] -= metAmount;
+        balanceOf[buyer] += metAmount;
+        emit Transfer(houseWallet, buyer, metAmount);
+        return true;
+    }
+
+    // Lose bet: transfer tokens from player to house wallet
+    function loseBet(address player, uint256 betAmount) public returns (bool success) {
+        require(balanceOf[player] >= betAmount, "Insufficient balance");
+        balanceOf[player] -= betAmount;
+        balanceOf[houseWallet] += betAmount;
+        emit Transfer(player, houseWallet, betAmount);
         return true;
     }
 }
