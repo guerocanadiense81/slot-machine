3c309329f18b1e94969295905580a21ba35c1521
guerocanadiense81 <cedrickp1981@gmail.com>
2025-03-30 06:29:20 -0400

    Update wallet.js

--- js/wallet.js
+++ js/wallet.js
@@ -1,7 +1,7 @@
+// /js/wallet.js
 window.addEventListener("load", async () => {
   if (typeof window.ethereum !== "undefined") {
     const connectBtn = document.getElementById("connectWallet");
-
     connectBtn?.addEventListener("click", async () => {
       try {
         await window.ethereum.request({ method: "eth_requestAccounts" });
